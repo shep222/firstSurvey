10c99e69c90d97f4f6268be3c9bc3b30e509d668
John Shepler <johnshepler@JohnsMac.local>
2018-01-09 13:47:13 -0700

    added more questions

--- main.js
+++ main.js
@@ -1,15 +1,36 @@
+
+var currentView = 'one'
+
 var vetStatus = true;
+var mortgage =  true;
+var homeType = "Single Family";
 var width = 10;
 
 
-function selected(answer) {
+function isVet(answer) {
   vetStatus = answer;
+  next()
+}
+
+function hasMortgage(answer) {
+  mortgage = answer;
+  next()
+}
+
+function homeTypes(answer) {
+  homeType = answer;
   next()
 }
 
 
 function next(){
-   console.log(vetStatus);
+   console.log(vetStatus + " " + mortgage + " " + homeType);
+
    var element = document.getElementById("myProgress");
    element.style.width = (width += 10) + '%';
+}
+
+function back(){
+   var element = document.getElementById("myProgress");
+   element.style.width = (width -= 10) + '%';
 }
