115db58e280463355cbe2324f6890cb73dbfd599
John Shepler <johnshepler@JohnsMac.local>
2018-01-09 14:58:16 -0700

    hidden/visible fields

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
-
+var lastView
 var currentView = 'one'
+var width = 10;
+
 
 var vetStatus = true;
 var mortgage =  true;
 var homeType = "Single Family";
-var width = 10;
 
 
 function isVet(answer) {
@@ -24,13 +25,36 @@ function homeTypes(answer) {
 
 
 function next(){
-   console.log(vetStatus + " " + mortgage + " " + homeType);
-
    var element = document.getElementById("myProgress");
    element.style.width = (width += 10) + '%';
+
+   if(currentView === 'one'){
+     lastView = 'one'
+     currentView = 'two'
+   } else if (currentView === 'two'){
+     lastView = 'two'
+     currentView = 'three'
+   }
+   // if(currentView !== 'one'){
+   //   document.getElementById(back).style.display = "block";
+   // }
+   document.getElementById(lastView).style.display = "none";
+   document.getElementById(currentView).style.display = "block";
 }
 
 function back(){
-   var element = document.getElementById("myProgress");
-   element.style.width = (width -= 10) + '%';
+  if(currentView === 'three'){
+    currentView = 'two'
+    lastView = 'three'
+  } else if (currentView === 'two') {
+    currentView = 'one'
+    lastView = 'two'
+  }
+  document.getElementById(lastView).style.display = "none";
+  document.getElementById(currentView).style.display = "block";
+
+  if(width > 10){
+    var element = document.getElementById("myProgress");
+    element.style.width = (width -= 10) + '%';
+  }
 }
