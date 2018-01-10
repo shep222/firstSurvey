c327c592674804582444ee333e6483ca1e991393
John Shepler <johnshepler@JohnsMac.local>
2018-01-10 11:00:55 -0700

    adding slider

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+$( function() {
+   $( "#slider" ).slider({
+     value:100,
+     min: 0,
+     max: 500,
+     step: 50,
+     slide: function( event, ui ) {
+       $( "#amount" ).val( "$" + ui.value );
+     }
+   });
+   $( "#amount" ).val( "$" + $( "#slider" ).slider( "value" ) );
+ } );
+
+
 var lastView
 var currentView = 'one'
 var width = 10;
