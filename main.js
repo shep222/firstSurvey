a1a547dd801ee641a0af9aff1a5e83e4bbb8c54d
John Shepler <johnshepler@JohnsMac.local>
2018-01-16 11:47:09 -0700

    fix sliders displays

--- main.js
+++ main.js
@@ -1,30 +1,58 @@
 var yes = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eighteen"]
 var no = ["one", "two", "three", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "eight", "sixteen", "nine", "seventeen", "ten", "eighteen"]
-var slider_values = [85000, 90000, 95000, 100000, 105000, 110000, 115000, 120000, 125000, 130000, 135000, 140000, 145000, 150000, 155000, 160000, 165000, 170000, 175000, 180000, 185000, 190000, 195000, 200000, 210000, 220000, 230000, 240000, 250000, 260000, 270000, 280000, 290000, 300000, 310000, 320000, 330000, 340000, 350000, 360000, 370000, 380000, 390000, 400000, 420000, 440000, 460000, 480000, 500000, 520000, 540000, 560000, 580000, 600000, 620000, 640000, 660000, 680000, 700000, 720000, 740000, 760000, 780000, 800000, 820000, 840000, 860000, 880000, 900000, 920000, 940000, 960000, 980000, 1000000, 1500000, 2000000]
 $(document).ready(function() {
+
 
   $('.answerBox').on('click', function(e) {
     $(this).find('input').prop("checked", true);
   });
+
+  $("form").submit(function(event) {
+    var str = $("form").serialize();
+    console.log(str);
+  })
+
+  $("myForm").validate({
+    rules: {
+      email: 'required'
+      // fName: "required",
+      // lName: "required",
+      // primaryPhone: "required",
+      // currentAddress: "required"
+    }
+  })
 })
+var estPropValues = ["75,000", "80,000", "85,000", "90,000", "95,000", "100,000", "105,000", "110,000", "115,000", "120,000", "125,000", "130,000", "135,000", "140,000", "145,000", "150,000", "155,000", "160,000", "165,000", "170,000", "175,000", "180,000", "185,000", "190,000", "195,000", "200,000", "210,000", "220,000", "230,000", "240,000", "250,000", "260,000", "270,000", "280,000", "290,000", "300,000", "310,000", "320,000", "330,000", "340,000", "350,000", "360,000", "370,000", "380,000", "390,000", "400,000", "420,000", "440,000", "460,000", "480,000", "500,000", "520,000", "540,000", "560,000", "580,000", "600,000", "620,000", "640,000", "660,000", "680,000", "700,000", "720,000", "740,000", "760,000", "780,000", "800,000", "820,000", "840,000", "860,000", "880,000", "900,000", "920,000", "940,000", "960,000", "980,000", "1,000,000", "1,000,001"]
+
+function stripedValue(allValues, value, type) {
+  var postedValue = allValues[value].replace (/,/g, "");
+    $("#" + type).val(postedValue);
+    console.log($("#" + type).val());
+}
 
 $(function() {
   $("#slider").slider({
-    value: 250000,
-    min: 75000,
-    max: 1000000,
-    step: 5000,
+    value: 30,
+    max: estPropValues.length - 1,
     slide: function(event, ui) {
-      $("#est_property_value").val("$" + ui.value);
+      stripedValue(estPropValues, ui.value, 'est_property_value')
+      if (ui.value === estPropValues.length - 1) {
+        $("#est_property_display").val("$" + estPropValues[ui.value - 1] + " +");
+      } else {
+        $("#est_property_display").val("$" + estPropValues[ui.value] + " -  $" + estPropValues[ui.value + 1]);
+      }
     }
   });
   $("#slider2").slider({
-    value: 155000,
-    min: 75000,
-    max: 1000000,
-    step: 5000,
+    value: 15,
+    max: estPropValues.length - 1,
     slide: function(event, ui) {
-      $("#mortgage_amount").val("$" + ui.value);
+      stripedValue(estPropValues, ui.value, 'mortgage_amount')
+      if (ui.value === estPropValues.length - 1) {
+        $("#mortgage_amount_display").val("$" + estPropValues[ui.value - 1] + " +");
+      } else {
+        $("#mortgage_amount_display").val("$" + estPropValues[ui.value] + " -  $" + estPropValues[ui.value + 1]);
+      }
     }
   });
   $("#slider3").slider({
@@ -37,19 +65,21 @@ $(function() {
     }
   });
   $("#slider4").slider({
-    value: 140000,
-    min: 75000,
-    max: 1000000,
-    step: 5000,
+    value: 13,
+    max: estPropValues.length - 1,
     slide: function(event, ui) {
-      $("#est_purchase_price").val("$" + ui.value);
+      stripedValue(estPropValues, ui.value, 'est_purchase_price')
+      if (ui.value === estPropValues.length - 1) {
+        $("#est_purchase_price_display").val("$" + estPropValues[ui.value] + " +");
+      } else {
+        $("#est_purchase_price_display").val("$" + estPropValues[ui.value] + " -  $" + estPropValues[ui.value + 1]);
+      }
     }
   });
-
-  $("#est_property_value").val("$" + $("#slider").slider("value"));
-  $("#mortgage_amount").val("$" + $("#slider2").slider("value"));
+  $("#est_property_display").val("$" + estPropValues[30] + " - $" + estPropValues[31]);
+  $("#mortgage_amount_display").val("$" + estPropValues[16] + " - $" + estPropValues[17]);
   $("#mortgage_rate").val("$" + $("#slider3").slider("value"));
-  $("#est_purchase_price").val("$" + $("#slider4").slider("value"));
+  $("#est_purchase_price_display").val("$" + estPropValues[13] + " - $" + estPropValues[14]);
 });
 
 var lastView
@@ -62,15 +92,26 @@ function hasMortgage(answer) {
   next()
 }
 
+function validateForm() {
+  var frame = $('section:visible');
+  frame.find('input').each(function() {
+    // console.log('yes');
+  })
+}
+
+
 function next() {
-  var viewPath = yes
+  validateForm();
+  var viewPath;
   var myView = currentView
 
   if (mortgage === true) {
-    viewpath = yes
+    viewPath = yes
   } else {
     viewPath = no
   }
+
+
   for (i = 0; i < viewPath.length - 1; i++) {
     if (viewPath[i] === myView) {
       currentView = viewPath[i + 1]
@@ -126,7 +167,3 @@ function back() {
     element.style.width = (width -= (viewPath.length - 2)) + '%';
   }
 }
-
-$("form").submit(function(event) {
-  console.log('here');
-})
