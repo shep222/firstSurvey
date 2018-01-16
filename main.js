95b71e83c93a45ac289fa0bba51b47943f38b752
John Shepler <johnshepler@JohnsMac.local>
2018-01-16 15:26:14 -0700

    setting up validation

--- main.js
+++ main.js
@@ -8,26 +8,27 @@ $(document).ready(function() {
   });
 
   $("form").submit(function(event) {
+    validateForm()
     var str = $("form").serialize();
     console.log(str);
   })
 
-  $("myForm").validate({
-    rules: {
-      email: 'required'
-      // fName: "required",
-      // lName: "required",
-      // primaryPhone: "required",
-      // currentAddress: "required"
-    }
-  })
+  // $("#vaForm").validate({
+  //   rules: {
+  //     // email: 'required'
+  //     // fName: "required",
+  //     // lName: "required",
+  //     // primaryPhone: "required",
+  //     // currentAddress: "required"
+  //   }
+  // })
 })
 var estPropValues = ["75,000", "80,000", "85,000", "90,000", "95,000", "100,000", "105,000", "110,000", "115,000", "120,000", "125,000", "130,000", "135,000", "140,000", "145,000", "150,000", "155,000", "160,000", "165,000", "170,000", "175,000", "180,000", "185,000", "190,000", "195,000", "200,000", "210,000", "220,000", "230,000", "240,000", "250,000", "260,000", "270,000", "280,000", "290,000", "300,000", "310,000", "320,000", "330,000", "340,000", "350,000", "360,000", "370,000", "380,000", "390,000", "400,000", "420,000", "440,000", "460,000", "480,000", "500,000", "520,000", "540,000", "560,000", "580,000", "600,000", "620,000", "640,000", "660,000", "680,000", "700,000", "720,000", "740,000", "760,000", "780,000", "800,000", "820,000", "840,000", "860,000", "880,000", "900,000", "920,000", "940,000", "960,000", "980,000", "1,000,000", "1,000,001"]
-var rateArray = ["2.75", "3.00"," 3.25"," 3.50"," 3.75", "4.00", "4.25", "4.50", "4.75", "5.00", "5.25"," 5.50", "5.75", "6.00", "6.25", "6.50", "6.75", "7.00", "7.25", "7.50", "7.75", "8.00"]
+var rateArray = ["2.75", "3.00", " 3.25", " 3.50", " 3.75", "4.00", "4.25", "4.50", "4.75", "5.00", "5.25", " 5.50", "5.75", "6.00", "6.25", "6.50", "6.75", "7.00", "7.25", "7.50", "7.75", "8.00"]
 
 function stripedValue(allValues, value, type) {
-  var postedValue = allValues[value].replace (/,/g, "");
-    $("#" + type).val(postedValue);
+  var postedValue = allValues[value].replace(/,/g, "");
+  $("#" + type).val(postedValue);
 }
 
 $(function() {
@@ -57,7 +58,7 @@ $(function() {
   });
   $("#slider3").slider({
     value: 8,
-    max: rateArray.length -1,
+    max: rateArray.length - 1,
     slide: function(event, ui) {
       stripedValue(rateArray, ui.value, 'mortgage_rate')
       $("#mortgage_rate_display").val(rateArray[ui.value] + "%");
@@ -97,8 +98,28 @@ function hasMortgage(answer) {
 
 function validateForm() {
   var frame = $('section:visible');
-  frame.find('input').each(function() {
-    // console.log('yes');
+  frame.find('input[type=text]').each(function() {
+    var isVal = $('input[type=text]').val()
+    console.log(isVal);
+    if (isVal !== "") {
+      isVal = ""
+      return
+    } else {
+      alert("NO NO!")
+
+    }
+  })
+
+  frame.find('input[type=email]').each(function() {
+    var emailVal = $('input[type=email]').val()
+    var atSymbol = emailVal.indexOf("@");
+    var dotSymbol = emailVal.lastIndexOf('.');
+    if (emailVal < 1 || dotSymbol < atSymbol + 2 || dotSymbol + 2 > emailVal.length) {
+      alert("NOT A VALID EMAIL!")
+      console.log(emailVal);
+    } else {
+      return;
+    }
   })
 }
 
