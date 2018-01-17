c995694312379ef0e38fc80dd8810e72c69a3a3c
John Shepler <johnshepler@JohnsMac.local>
2018-01-17 10:22:59 -0700

    validations complete

--- main.js
+++ main.js
@@ -7,11 +7,20 @@ $(document).ready(function() {
     $(this).find('input').prop("checked", true);
   });
 
-  $("form").submit(function(event) {
+
+  $("#submit").on("click", function(e) {
+    console.log(valid);
     validateForm()
-    var str = $("form").serialize();
-    console.log(str);
+    console.log(valid);
+    if (valid === true) {
+      $("form").submit(function(event) {
+        var str = $("form").serialize();
+        console.log(str);
+      })
+    }
   })
+
+
 
 })
 var estPropValues = ["75,000", "80,000", "85,000", "90,000", "95,000", "100,000", "105,000", "110,000", "115,000", "120,000", "125,000", "130,000", "135,000", "140,000", "145,000", "150,000", "155,000", "160,000", "165,000", "170,000", "175,000", "180,000", "185,000", "190,000", "195,000", "200,000", "210,000", "220,000", "230,000", "240,000", "250,000", "260,000", "270,000", "280,000", "290,000", "300,000", "310,000", "320,000", "330,000", "340,000", "350,000", "360,000", "370,000", "380,000", "390,000", "400,000", "420,000", "440,000", "460,000", "480,000", "500,000", "520,000", "540,000", "560,000", "580,000", "600,000", "620,000", "640,000", "660,000", "680,000", "700,000", "720,000", "740,000", "760,000", "780,000", "800,000", "820,000", "840,000", "860,000", "880,000", "900,000", "920,000", "940,000", "960,000", "980,000", "1,000,000", "1,000,001"]
@@ -89,14 +98,29 @@ function hasMortgage(answer) {
 
 function validateForm() {
   var frame = $('section:visible');
-  frame.find('input[type=text]').each(function() {
-    var isVal = $(this).val()
-    console.log(isVal);
-    if (isVal !== "") {
+
+  frame.find('input[type=number]').each(function() {
+    var isZip = $(this).val()
+    if (isZip !== "" && isZip.length === 5) {
       isVal = ""
+      $(this).removeClass("warning")
       valid = true
     } else {
-       valid = false
+      $(this).addClass("warning")
+      $(this).focus()
+      valid = false
+    }
+  })
+
+  frame.find('input[type=text]').each(function() {
+    var isFilled = $(this).val()
+    if (isFilled.length > 0) {
+      isFilled = ""
+      valid = true
+    } else {
+      valid = false
+      $(this).addClass("warning");
+      $(this).focus()
     }
   })
 
@@ -105,9 +129,12 @@ function validateForm() {
     var atSymbol = emailVal.indexOf("@");
     var dotSymbol = emailVal.lastIndexOf('.');
     if (emailVal < 1 || dotSymbol < atSymbol + 2 || dotSymbol + 2 > emailVal.length) {
-       valid = false;
+      $(this).addClass("warning")
+      $(this).focus()
+      valid = false;
     } else {
-       valid = true;
+      $(this).removeClass("warning")
+      valid = true;
     }
   })
 }
@@ -124,35 +151,35 @@ function next() {
     viewPath = no
   }
 
-console.log(valid);
-if (valid === true) {
-  for (i = 0; i < viewPath.length - 1; i++) {
-    if (viewPath[i] === myView) {
-      currentView = viewPath[i + 1]
-      lastView = myView
+  if (valid === true) {
+    for (i = 0; i < viewPath.length - 1; i++) {
+      if (viewPath[i] === myView) {
+        currentView = viewPath[i + 1]
+        lastView = myView
+      }
     }
-  }
-  if (currentView !== 'one') {
-    document.getElementById("back").style.display = "block";
-  }
-  if (currentView === 'eighteen') {
-    document.getElementById("submit").style.display = "block";
-    document.getElementById("next").style.display = "none";
+    if (currentView !== 'one') {
+      document.getElementById("back").style.display = "block";
+    }
+    if (currentView === 'eighteen') {
+      document.getElementById("submit").style.display = "block";
+      document.getElementById("next").style.display = "none";
+    } else {
+      document.getElementById("submit").style.display = "none";
+      document.getElementById("next").style.display = "block";
+    }
+    document.getElementById(lastView).style.display = "none";
+    document.getElementById(currentView).style.display = "block";
+    var element = document.getElementById("myProgress");
+    element.style.width = (width += (100 / viewPath.length)) + '%';
   } else {
-    document.getElementById("submit").style.display = "none";
-    document.getElementById("next").style.display = "block";
+    return
   }
-  document.getElementById(lastView).style.display = "none";
-  document.getElementById(currentView).style.display = "block";
-  var element = document.getElementById("myProgress");
-  element.style.width = (width += (100 / viewPath.length)) + '%';
-} else {
-  return
-}
 
 }
 
 function back() {
+  valid = true
   var viewPath = yes
   var myView = currentView
   if (mortgage === true) {
